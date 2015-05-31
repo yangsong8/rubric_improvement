e579b10277d5ab004401660da1fcff2f5ec0d420
Yang Song <ysong8@ncsu.edu>
2015-05-31 13:22:25 -0400

    Update 20150531171005_create_multiple_tables.rb

--- db/migrate/20150531171005_create_multiple_tables.rb
+++ db/migrate/20150531171005_create_multiple_tables.rb
@@ -1,6 +1,32 @@
 class CreateMultipleTables < ActiveRecord::Migration
-  def change
-    create_table :multiple_tables do |t|
+  def self.up
+    create_table :assignments do |t|
+      t.integer :questionnaire_id
+      t.string :name
+      t.integer :course_id
+      t.string :course_name
+      t.integer :student_num
+      t.integer :reviewer_num
+      t.integer :review_num
+      t.float :overall_reliability
+    end
+    
+    create_table :question_quality do |t|
+      t.integer :question_id
+      t.integer :assignment_id
+      t.float :overall_reliability
+    end
+
+    create_table :questions do |t|
+      t.text :txt
+      t.integer :questionnaire_id
+    end
+
+    create_table :reviewee_teams do |t|
+      t.integer :assignment_id
+      t.float :overall_reliability
+    end
+
     end
   end
 end
